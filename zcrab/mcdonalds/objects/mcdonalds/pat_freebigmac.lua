6cdf4f23c250dde4b6244771b9f9a5785e26c880
patman <27874300+patmanf@users.noreply.github.com>
2022-06-10 14:32:00 -0500

    give the new mcdonalds big mac instead of a vanilla burger

--- zcrab/mcdonalds/objects/mcdonalds/pat_freebigmac.lua
+++ zcrab/mcdonalds/objects/mcdonalds/pat_freebigmac.lua
@@ -2,8 +2,8 @@ table.insert(interactFunctions, function(args, data)
 	if math.random(1, 200) == 1 then
 		local pos = world.entityPosition(args.sourceId) or entity.position()
 		local params = {price = 0}
-		world.spawnItem("hamburger", pos, 1, params, {0, 0}, 0)
-		world.spawnItem("zcrab_mcdonaldsdrink", pos, 1, params, {0, 0}, 0)
+		world.spawnItem("zcrab_bigmac", pos, 1, params, nil, 0)
+		world.spawnItem("zcrab_mcdonaldsdrink", pos, 1, params, nil, 0)
 		object.say("you win Free big mac and the drink!")
 	end
 end)
